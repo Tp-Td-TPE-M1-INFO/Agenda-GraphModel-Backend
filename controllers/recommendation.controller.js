f55622a44718bc076fdd15b863088d6eb0668008
juniortchinde <junior.tchinde@facsciences-uy1.cm>
2023-01-04 13:01:55 +0100

    recommendation de nourriture terminée

--- controllers/recommendation.controller.js
+++ controllers/recommendation.controller.js
@@ -1,6 +1,7 @@
 const User = require('../model/user.model');
 const Nutrition = require('../model/nutrition.model');
 const Food = require('../model/food.model');
+const HeathProblem = require('../model/heathProblem.model');
 
 module.exports.foodRecommendation = async (req, res) => {
     const user = User.findOne({_id: req.params.id});
@@ -18,38 +19,36 @@ module.exports.foodRecommendation = async (req, res) => {
         {
             for( j in nutritions[i].foods){
                 let foodT = await Food.findById({_id: nutritions[i].foods[j]})
-                console.log(foodT)
                 glucideScore += foodT.glucide;
                 proteineScore += foodT.proteine;
                 lipideScore += foodT.lipide;
                 selScore += foodT.sel;
-                console.log(j)
             }
         }
         
         for(j in user.illness){
-
-            glucideScore +=  user.illness;
-            proteineScore += user.illness;
-            lipideScore += user.illness;
-            selScore += user.illness[j].sel;
+            let illness = await HeathProblem.findById({_id: user.illness[j]})
+            glucideScore +=  illness.glucide;
+            proteineScore += illness.proteine;
+            lipideScore += illness.lipide;
+            selScore += illness.sel;
         }
         
         if(glucideScore <= 200) foods.push( await Food.find({ glucide:{$gt: 70}}).limit(3).sort({'glucide': -1}));
         else if(glucideScore >= 1000) foods.push( await Food.find({ glucide:{$lt: 10}}).limit(3).sort({'glucide': 1}));
-        else if(glucideScore > 200 && glucideScore < 5000) foods.push( await Food.find({ glucide:{$gt: 20, $lt: 60}}).limit(3));
+        else if(glucideScore > 200 && glucideScore < 1000) foods.push( await Food.find({ glucide:{$gt: 20, $lt: 60}}).limit(3));
 
         if(proteineScore <= 200) foods.push( await Food.find({ proteine:{$gt: 70}}).limit(3).sort({'proteine': -1}));
-        else if(proteineScore >= 1500) foods.push(Food.find({ proteine:{$lt: 10}}).limit(3).sort({'proteine': 1}));
-        else if(proteineScore >200 && proteineScore < 5000) foods.push(await Food.find({ proteine:{$gt: 20, $lt: 60}}).limit(3));
+        else if(proteineScore >= 1000) foods.push(Food.find({ proteine:{$lt: 10}}).limit(3).sort({'proteine': 1}));
+        else if(proteineScore >200 && proteineScore < 1000) foods.push(await Food.find({ proteine:{$gt: 20, $lt: 60}}).limit(3));
 
         if(lipideScore <= 200) foods.push(await Food.find({ lipide:{$gt: 70}}).limit(3).sort({'lipide': -1}));
-        else if(lipideScore >= 1500) foods.push(await Food.find({ lipide:{$lt: 10}}).limit(3).sort({'lipide': 1}));
-        else if(lipideScore > 200 && lipideScore < 5000) foods.push(await Food.find({ lipide:{$gt: 20, $lt: 60}}).limit(3));
+        else if(lipideScore >= 1000) foods.push(await Food.find({ lipide:{$lt: 10}}).limit(3).sort({'lipide': 1}));
+        else if(lipideScore > 200 && lipideScore < 1000) foods.push(await Food.find({ lipide:{$gt: 20, $lt: 60}}).limit(3));
     
         if(selScore <= 100) foods.push(await Food.find({ sel:{$gt: 30}}).limit(3).sort({'sel': -1}));
         else if(selScore >= 1000) foods.push(await Food.find({ sel:{$lt: 5}}).limit(3).sort({'sel': 1}));
-        else if(selScore > 100 && selScore < 3000) foods.push(await Food.find({ sel:{$gt: 20, $lt: 40}}).limit(3));
+        else if(selScore > 100 && selScore < 1000) foods.push(await Food.find({ sel:{$gt: 20, $lt: 40}}).limit(3));
 
        return res.status(200).json(foods);
     }
